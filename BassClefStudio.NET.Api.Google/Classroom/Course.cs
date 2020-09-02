75d5cf65b0094bdf4e532bb901287b9fb5bc6147
bassclefstudio <51424093+bassclefstudio@users.noreply.github.com>
2020-09-01 22:32:20 -0400

    Added extremely basic Google Classroom APIs in new project.

---
+++ BassClefStudio.NET.Api.Google/Classroom/Course.cs
@@ -0,0 +1,17 @@
+﻿using BassClefStudio.NET.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BassClefStudio.NET.Api.Google.Classroom
+{
+    public class Course : IIdentifiable<string>
+    {
+        public string Id { get; }
+
+        public Course(string id)
+        {
+            Id = id;
+        }
+    }
+}
